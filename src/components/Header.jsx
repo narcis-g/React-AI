fd3b8201ef9662cda578f95651c4f175fd181b2c
narcis-g <narcis_alexinho@yahoo.com>
2023-08-09 11:38:30 +0300

    Add the style for the h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import {logo} from '../assets'
-
+import React from "react";
+import { logo } from "../assets";
 
 const Header = () => {
   return (
-    <header className ="w-full flex 
-                    justify-center items-center flex-col">
-        <nav className="flex justify-between items-center flex-col">
-          <img src={logo}></img>
-          </nav>    
-
+    <header
+      className="w-full flex 
+                    justify-center items-center flex-col"
+    >
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={ logo } alt="sumz_logo" className="w-28 object-contain"></img>
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/narcis-g/React-AI")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Ask chatty for help
+        <br className="max-md:hidden" />
+        <span className="dark_blue_gradient">OpenAi GPT</span>
+      </h1>
+      {/* <h2 className="desc">
+        
+      </h2> */}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
