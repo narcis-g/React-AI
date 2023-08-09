3c94d4882dc77088aa9f8d5a80758c7e4d290f40
narcis-g <narcis_alexinho@yahoo.com>
2023-08-09 12:13:21 +0300

    Add the input field with the styling and without the logic

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Content = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="mt-16 w-full max-w-wl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          ></img>
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          ></input>
+          <button
+            type="submit"
+            className="submit_btn 
+                      peer-focus:border-grey-700 
+                      peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
+        </form>
 
-export default Content
+        {/* Browse History */}
+      </div>
+
+      {/* Display results */}
+    </section>
+  );
+};
+
+export default Content;
