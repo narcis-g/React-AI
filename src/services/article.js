7a62373e81f0b2eeb5ac852b657e4916700b1aa7
narcis-g <narcis_alexinho@yahoo.com>
2023-08-15 19:21:50 +0300

    Add the link for the api that I use

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,11 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+// https://rapidapi.com/restyler/api/article-extractor-and-summarizer link for the api
 export const articleApi = createApi ({
     reducerPath:'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: ''
+    }),
     endpoints:(builder) => ({
         getSummary: builder.query({
             query: (params) => 'test'
