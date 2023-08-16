7b3abc8115a9a47e020c804b3e4e338fb4d55c19
narcis-g <narcis_alexinho@yahoo.com>
2023-08-16 20:36:43 +0300

    Add the api setup from the rapidApi site

--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,18 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 // https://rapidapi.com/restyler/api/article-extractor-and-summarizer link for the api
+
+const rapidApiKey= import.meta.env.VITE_RAPID_API_ARTICLE_KEY
+
 export const articleApi = createApi ({
     reducerPath:'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: ''
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey)
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com')
+
+        }
     }),
     endpoints:(builder) => ({
         getSummary: builder.query({
