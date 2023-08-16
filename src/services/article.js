45652cc90bcf53d98824e0ec25970f784ebeb2c7
narcis-g <narcis_alexinho@yahoo.com>
2023-08-16 20:44:19 +0300

    Finishing the setup for the api

--- src/services/article.js
+++ src/services/article.js
@@ -1,23 +1,29 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // https://rapidapi.com/restyler/api/article-extractor-and-summarizer link for the api
 
-const rapidApiKey= import.meta.env.VITE_RAPID_API_ARTICLE_KEY
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-export const articleApi = createApi ({
-    reducerPath:'articleApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey)
-            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com')
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
 
-        }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `/summarize?url=${encodeURIComponent(params.articleUrl)}&lenngth=3`,
     }),
-    endpoints:(builder) => ({
-        getSummary: builder.query({
-            query: (params) => 'test'
-        })
-    })
-
+  }),
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
